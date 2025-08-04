257c3d3ae4a105a383dcde4004043da891c60637
Sarthak2177 <sarthak.22310427@viit.ac.in>
2025-08-05 02:38:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-uvicorn main:app --host 0.0.0.0 --port $PORT
+# start.sh
+
+# Ensure .env variables are loaded if needed
+if [ -f ".env" ]; then
+  export $(cat .env | xargs)
+fi
+
+# Start the FastAPI app
+uvicorn main:app --host 0.0.0.0 --port 10000
